d7cacf70277e6a8ba9c607f29d60859453ff6992
Rodolfo <rodolfojr94@gmail.com>
2022-03-10 20:23:55 -0300

    refactor: Update Readme and add improvements to main.py

--- README.md
+++ README.md
@@ -1,10 +1,65 @@
-arquivo .env
+## Installation
+
+- Create the virtual environment
+
+  ```bash
+    virtualenv -p python3 venv
+  ```
+
+- Activate environment 
+
+  ```bash
+    source venv/bin/activate
+  ```
+
+- Install the requirements
+
+  ```bash
+  pip install -r requirements.txt
+  ```
+
+- Install playwright requirements
+
+  ```bash
+  playwright install
+  ```
+
+  - If that doesn't work, the following command will install the required dependencies:
+
+    ```bash
+    playwright install-deps
+    ```
+
+## Run
+
+- To run, in the root folder, type:
+
+```shell
+python main.py <file-path>
+```
+
+- example
+
+  ```
+  python main.py ./profiles.csv
+  ```
+
+## Required input files exemples
+
+#### .env file
+
+```.env
 EMAIL=email@email.com
 PASSWORD=senha123
+```
 
-arquivo csv
+#### CSV file
+
+```csv
 email
 https://www.linkedin.com/in/profile1/
 https://www.linkedin.com/in/profile2/
 https://www.linkedin.com/in/profile3/
 https://www.linkedin.com/in/profile4/
+```
+
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+#!/usr/bin/env python3
 from playwright.sync_api import Playwright, sync_playwright
 
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import csv
+import sys
 
 
-def run(playwright: Playwright, profile_links: list[str]) -> None:
-    browser = playwright.chromium.launch(headless=False, timeout=10000)
+def run(playwright: Playwright, profile_links: list[str], timeout=10000):
+    browser = playwright.chromium.launch(headless=False, timeout=timeout)
     context = browser.new_context()
 
-    # Open new page
     page = context.new_page()
-
-    # Go to https://www.linkedin.com/
     page.goto("https://www.linkedin.com/")
 
     dotenv_path = join(dirname(__file__), '.env')
@@ -22,43 +21,35 @@ def run(playwright: Playwright, profile_links: list[str]) -> None:
     EMAIL = os.environ.get('EMAIL')
     PASSWORD = os.environ.get('PASSWORD')
 
-    # Click input[name="session_key"]
     page.locator("input[name=\"session_key\"]").click()
-
-    # Fill input[name="session_key"]
     page.locator("input[name=\"session_key\"]").fill(EMAIL)
 
-    # Click text=Senha Exibir >> [placeholder="\ "]
     page.locator("text=Senha Exibir >> [placeholder=\"\\ \"]").click()
-
-    # Fill text=Senha Exibir >> [placeholder="\ "]
     page.locator("text=Senha Exibir >> [placeholder=\"\\ \"]").fill(PASSWORD)
 
-    # Click text=Entrar >> nth=1
-    # with page.expect_navigation(url="https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit"):
     with page.expect_navigation():
         page.locator("text=Entrar").nth(1).click()
-    # assert page.url == "https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit"
 
     for profile in profile_links:
-        # Open new page
         newPage = context.new_page()
-        # Go to https://www.linkedin.com/in/profile/
         newPage.goto(profile)
-        # Close page
         newPage.close()
 
-    # ---------------------
     context.close()
     browser.close()
 
 
 def read_profiles_file(filename) -> list[str]:
     with open(filename) as file:
-        data = csv.reader(file)
-        return [profile_line[0] for profile_line in data][1:]
+        file_data = csv.reader(file)
+        header, *profiles = file_data
+        email_index = header.index('email')
+        return [profile[email_index] for profile in profiles]
 
 
 with sync_playwright() as playwright:
-    profiles = read_profiles_file('profiles.csv')
-    run(playwright, profiles)
+    file_path = sys.argv[-1]
+
+    profiles = read_profiles_file(file_path)
+
+    run(playwright, profiles, 10000)
